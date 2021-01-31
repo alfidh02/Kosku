25337681931c39d5f09ea1b1a559562fbbf4ffdc
alfidh02 <alfidarmawan79@gmail.com>
2021-01-31 21:32:40 +0700

    :rocket: add firebase & form

--- app/src/main/java/com/project/kosku/MainActivity.kt
+++ app/src/main/java/com/project/kosku/MainActivity.kt
@@ -3,6 +3,7 @@ package com.project.kosku
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import com.project.kosku.fragment.CostFragment
 import com.project.kosku.fragment.HomeFragment
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         if (savedInstanceState == null) {
             btmNavbar.setItemSelected(R.id.home, true)
--- app/src/main/java/com/project/kosku/fragment/HomeFragment.kt
+++ app/src/main/java/com/project/kosku/fragment/HomeFragment.kt
@@ -1,34 +1,20 @@
 package com.project.kosku.fragment
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.DatePicker
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import com.project.kosku.R
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.layout_dialog.*
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class HomeFragment : Fragment(), DatePickerDialog.OnDateSetListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +24,60 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var locale = Locale("id", "ID")
+
+        tvMonth.setText(
+            Calendar.getInstance().getDisplayName(Calendar.MONTH, Calendar.LONG, locale)
+        )
+        tvYear.setText(Calendar.getInstance().get(Calendar.YEAR).toString())
+
+        fabAdd.setOnClickListener {
+            dialogAdd()
+        }
+
     }
+
+    private fun dialogAdd() {
+        val view: View = layoutInflater.inflate(R.layout.layout_dialog, null)
+        val pickDate : Button = view.findViewById(R.id.btnDate)
+
+        val dialog = AlertDialog.Builder(context!!)
+        dialog.apply {
+            setTitle("Data Penyewa")
+            setNegativeButton("Batal") { dialogInterface, i ->
+                dialogInterface.dismiss()
+            }
+            setPositiveButton("Tambah") { dialogInterface, i ->
+
+            }
+        }
+
+        pickDate.setOnClickListener {
+            showDatePicker()
+        }
+
+        dialog.setView(view)
+        dialog.show()
+    }
+
+    private fun showDatePicker() {
+
+        val datePickerDialog = DatePickerDialog (
+            context!!,
+            this,
+            Calendar.getInstance().get(Calendar.YEAR),
+            Calendar.getInstance().get(Calendar.MONTH),
+            Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        )
+
+        datePickerDialog.show()
+    }
+
+    override fun onDateSet(p0: DatePicker?, p1: Int, p2: Int, p3: Int) {
+        val date:String = "tanggal sekarang : " + p3 + "/" + p2+1 + "/" +p1
+    }
+
 }
