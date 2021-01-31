7effcbf4a73cdd5ed0409016bd360c63f9fda69d
alfidh02 <alfidarmawan79@gmail.com>
2021-01-31 16:03:21 +0700

    initial commit

---
+++ app/src/main/java/com/project/kosku/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.project.kosku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
