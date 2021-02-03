cc0d5f8080f7655c3e421ec7ce0b580b9c8c391a
alfidh02 <alfidarmawan79@gmail.com>
2021-02-03 22:33:32 +0700

    :rocket: add payment status to TenantActivity.kt

--- app/src/main/java/com/project/kosku/TenantActivity.kt
+++ app/src/main/java/com/project/kosku/TenantActivity.kt
@@ -1,24 +1,39 @@
 package com.project.kosku
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import com.project.kosku.model.Tenant
 import kotlinx.android.synthetic.main.activity_tenant.*
 
 class TenantActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tenant)
         setToolbar()
 
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
         val item = intent.getParcelableExtra<Tenant>("dataKos")
 
         tvName.setText(item!!.name)
         tvHp.setText(item!!.noHp)
         tvTggl.setText(item!!.tgglMasuk)
+
+        if (item!!.status == false) {
+            tvStatus.setTextColor(ResourcesCompat.getColor(resources, R.color.colorDanger, null))
+            tvStatus.setText("BELUM BAYAR")
+            btnBayar.visibility = View.VISIBLE
+        } else {
+            tvStatus.setTextColor(ResourcesCompat.getColor(resources, R.color.colorBtn, null))
+            tvStatus.setText("SUDAH BAYAR")
+        }
     }
 
     private fun setToolbar() {
--- app/src/main/java/com/project/kosku/adapter/HomeAdapter.kt
+++ app/src/main/java/com/project/kosku/adapter/HomeAdapter.kt
@@ -5,7 +5,6 @@ import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.project.kosku.R
@@ -30,15 +29,27 @@ class HomeAdapter(private var tenants: ArrayList<Tenant>) :
 
         holder.itemView.setOnClickListener {
             it.context.startActivity(
-                Intent(it.context, TenantActivity::class.java).putExtra("dataKos",tenant)
+                Intent(it.context, TenantActivity::class.java).putExtra("dataKos", tenant)
             )
         }
 
         if (tenant.status == false) {
-            holder.itemView.tvStatus.setTextColor(ResourcesCompat.getColor(holder.itemView.resources,R.color.colorDanger, null))
+            holder.itemView.tvStatus.setTextColor(
+                ResourcesCompat.getColor(
+                    holder.itemView.resources,
+                    R.color.colorDanger,
+                    null
+                )
+            )
             holder.itemView.tvStatus.text = "BELUM BAYAR"
         } else {
-            holder.itemView.tvStatus.setTextColor(ResourcesCompat.getColor(holder.itemView.resources,R.color.colorBtn, null))
+            holder.itemView.tvStatus.setTextColor(
+                ResourcesCompat.getColor(
+                    holder.itemView.resources,
+                    R.color.colorBtn,
+                    null
+                )
+            )
             holder.itemView.tvStatus.text = "SUDAH BAYAR"
         }
 
