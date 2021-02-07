c94df6822a05917387bf88d09efc52f0efffc7b3
alfidh02 <alfidarmawan79@gmail.com>
2021-02-07 21:58:22 +0700

    :rocket: add success animation

--- app/src/main/java/com/project/kosku/fragment/CostFragment.kt
+++ app/src/main/java/com/project/kosku/fragment/CostFragment.kt
@@ -1,17 +1,33 @@
 package com.project.kosku.fragment
 
 import android.animation.Animator
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.database.*
 import com.project.kosku.R
 import com.project.kosku.adapter.TabAdapter
+import com.project.kosku.model.Wallet
 import kotlinx.android.synthetic.main.fragment_cost.*
+import kotlinx.android.synthetic.main.layout_dialog.view.*
+import kotlinx.android.synthetic.main.layout_wallet.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class CostFragment : Fragment() {
+class CostFragment : Fragment(), DatePickerDialog.OnDateSetListener {
+    private lateinit var mFirebaseDatabase: DatabaseReference
+    private lateinit var mFirebaseInstance: FirebaseDatabase
     var isFabOpen :Boolean = false
+    lateinit var tNominal: String
+    lateinit var tDetail: String
+    lateinit var dateTransaction: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +40,9 @@ class CostFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        mFirebaseInstance = FirebaseDatabase.getInstance()
+        mFirebaseDatabase = mFirebaseInstance.getReference("Wallet")
+
         val adapter = TabAdapter(childFragmentManager, tabLayout.tabCount)
         adapter.addFragment(IncomeFragment(), "Pemasukan")
         adapter.addFragment(OutcomeFragment(), "Pengeluaran")
@@ -33,6 +52,76 @@ class CostFragment : Fragment() {
         fabAdd.setOnClickListener {
             if (!isFabOpen) showFabMenu() else closeFabMenu()
         }
+
+        fabLayout1.setOnClickListener {
+            dialogAdd()
+        }
+    }
+
+    private fun dialogAdd() {
+
+        val view: View = layoutInflater.inflate(R.layout.layout_wallet, null)
+        val pickDate: Button = view.findViewById(R.id.btnDate)
+
+        pickDate.setOnClickListener {
+            showDatePicker()
+        }
+
+        val dialog = AlertDialog.Builder(context!!)
+        dialog.apply {
+            setTitle("Data Kos")
+            setNegativeButton("Batal") { dialogInterface, i ->
+                dialogInterface.dismiss()
+            }
+            setPositiveButton("Tambah", null)
+        }
+
+        dialog.setView(view)
+
+        val alertDialog = dialog.create()
+        alertDialog.show()
+
+        val positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
+        positiveButton.setOnClickListener {
+            tNominal = view.etNominal.text.toString().trim()
+            tDetail = view.etDetail.text.toString().trim()
+
+            if (tNominal.equals("") && tDetail.equals("")) {
+                view.etName.error = "Nama tidak boleh kosong"
+                view.etNo.error = "No. HP masa tidak punya?!"
+            } else if (tNominal.equals("")) {
+                view.etName.error = "Nama tidak boleh kosong"
+                view.etName.requestFocus()
+            } else if (tDetail.equals("")) {
+                view.etNo.error = "No. HP masa tidak punya?!"
+                view.etNo.requestFocus()
+            } else {
+                saveOutcome(tNominal, tDetail, dateTransaction)
+                alertDialog.dismiss()
+            }
+        }
+    }
+
+    private fun saveOutcome(tNominal: String, tDetail: String, dateTransaction: String) {
+
+        val wallet = Wallet()
+        wallet.nominal = tNominal
+        wallet.detail = tDetail
+        wallet.date = dateTransaction
+        wallet.tipe = false
+
+        mFirebaseDatabase.child("Income").addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(p0: DataSnapshot) {
+                mFirebaseDatabase.child("Income").setValue(wallet)
+                succesAnimation()
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                Toast.makeText(context, "${p0.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
+
     }
 
     private fun showFabMenu() {
@@ -68,4 +157,37 @@ class CostFragment : Fragment() {
         })
     }
 
+    private fun showDatePicker() {
+
+        val datePickerDialog = DatePickerDialog(
+            context!!,
+            this,
+            Calendar.getInstance().get(Calendar.YEAR),
+            Calendar.getInstance().get(Calendar.MONTH),
+            Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        )
+
+        datePickerDialog.show()
+    }
+
+    override fun onDateSet(p0: DatePicker?, p1: Int, p2: Int, p3: Int) {
+        var calendar = Calendar.getInstance()
+        calendar.set(p1, p2, p3)
+
+        var dateFormat = SimpleDateFormat("dd/MM/yyyy")
+        dateTransaction = dateFormat.format(calendar.time)
+    }
+
+    private fun succesAnimation() {
+        val  dialogBuilder = AlertDialog.Builder(context!!)
+        val layoutView : View = layoutInflater.inflate(R.layout.layout_success, null)
+        val dialogButton : Button = layoutView.findViewById(R.id.btnDialog)
+        dialogBuilder.setView(layoutView)
+
+        val  alertDialog = dialogBuilder.create()
+        alertDialog.show()
+        dialogButton.setOnClickListener {
+            alertDialog.dismiss()
+        }
+    }
 }
