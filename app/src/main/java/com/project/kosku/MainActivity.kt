980138d3214d56053b385683f339791ad8eae66f
alfidh02 <alfidarmawan79@gmail.com>
2021-02-13 19:30:12 +0700

    :building_construction: change status bar

--- app/src/main/java/com/project/kosku/MainActivity.kt
+++ app/src/main/java/com/project/kosku/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.project.kosku
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -16,9 +18,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            this.window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+        }
 
         if (savedInstanceState == null) {
             btmNavbar.selectedItemId = R.id.home
@@ -26,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         btmNavbar.setOnNavigationItemSelectedListener {
-            when(it.itemId) {
+            when (it.itemId) {
                 R.id.home -> {
                     addFragment(HomeFragment())
                     return@setOnNavigationItemSelectedListener true
