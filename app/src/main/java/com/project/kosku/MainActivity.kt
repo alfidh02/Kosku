cdc5385697f6b972d2e61670e6a460ad09522f7f
alfidh02 <alfidarmawan79@gmail.com>
2021-03-02 18:53:09 +0700

    :boom: add bottom navbar padding for android 10

--- app/src/main/java/com/project/kosku/MainActivity.kt
+++ app/src/main/java/com/project/kosku/MainActivity.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.OnApplyWindowInsetsListener
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.project.kosku.fragment.CostFragment
 import com.project.kosku.fragment.HomeFragment
 import com.project.kosku.fragment.ReportFragment
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,7 +27,24 @@ class MainActivity : AppCompatActivity() {
         )
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             this.window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+            ViewCompat.setOnApplyWindowInsetsListener(
+                this.window.decorView,
+                object : OnApplyWindowInsetsListener {
+                    override fun onApplyWindowInsets(
+                        v: View,
+                        insets: WindowInsetsCompat
+                    ): WindowInsetsCompat? {
+                        v.setPadding(
+                            0,
+                            0,
+                            0,
+                            v.paddingBottom + insets.systemWindowInsetBottom - 40
+                        )
+                        return insets
+                    }
+                })
         }
+
 
         if (savedInstanceState == null) {
             btmNavbar.selectedItemId = R.id.home
